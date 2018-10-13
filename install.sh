75758557a22e6b23115695e89a2bcba8d08ea990
xavfan <33391012+xavfan@users.noreply.github.com>
2018-10-13 18:56:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ wd="/brewing-controller"
 objname="hlt-temp-sensor"
 objscript="$objname"".py"
 docker container rm hlt-temp-sensor
-docker run -ti --name $objname -w $wd -v $wd:$wd $base_image python $objscript
+docker run -ti --name $objname -w $wd -v $wd:$wd $base_image python $objscript titi
