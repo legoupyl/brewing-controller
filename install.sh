ed39e77ce6ad4fd674463cf052389a4f5ed1c7ab
xavfan <33391012+xavfan@users.noreply.github.com>
2018-10-13 18:07:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 base_image="xavfan/brew-base"
 docker container rm hlt-temp-sensor
-docker run -ti --name hlt-temp-sensor $base_image
+docker run -ti --name hlt-temp-sensor -v /brewing-controller:/brewing-controller $base_image /brewing-controller/hlt-temp-sensor.py
