b45c37426e5c1fe624ae40b0b971c35ac089325b
xavfan <xavier.fanton@hotmail.fr>
2019-12-11 21:59:42 +0100

    ok

--- gui/bc-gui.py
+++ gui/bc-gui.py
@@ -4,5 +4,5 @@ import tkinter as tk
 root = tk.Tk()
 img = Image.open("brasserie.bmp")
 tkimage = ImageTk.PhotoImage(img)
-tk.Label(root, image=tkimage).pack()
+tk.Label(root, image=tkimage,text="Hello, world!").pack()
 root.mainloop()
