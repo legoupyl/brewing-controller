59db68f292300ff578b3ccd98be0a24350675245
xavfan <xavier.fanton@hotmail.fr>
2019-12-11 21:28:30 +0100

    ok

--- gui/bc-gui.py
+++ gui/bc-gui.py
@@ -1,9 +1,16 @@
 from PIL import Image, ImageTk 
 import tkinter as tk 
 
+
+mlt_temp= 66
+bk_temp= 95
+hlt_temp= 65
 root = tk.Tk()
 root.attributes('-zoomed', True)
 img = Image.open("brasserie.bmp")
 tkimage = ImageTk.PhotoImage(img)
 tk.Label(root, image=tkimage).pack()
+
+mlt_temp_label = Label(root, text=mlt_temp)
+
 root.mainloop()
