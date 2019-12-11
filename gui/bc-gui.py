7f7db670373844ed2e784291f9850437f1b3d9c8
xavfan <xavier.fanton@hotmail.fr>
2019-12-11 21:54:13 +0100

    ok

--- gui/bc-gui.py
+++ gui/bc-gui.py
@@ -1,14 +1,9 @@
 from PIL import Image, ImageTk 
 import tkinter as tk 
 
-mlt_temp= 66
-bk_temp= 95
-hlt_temp= 65
 root = tk.Tk()
-#root.attributes('-zoomed', True)
+tk.attributes("-fullscreen", True) 
 img = Image.open("brasserie.bmp")
 tkimage = ImageTk.PhotoImage(img)
-
-
-
+tk.Label(root, image=tkimage).pack()
 root.mainloop()
