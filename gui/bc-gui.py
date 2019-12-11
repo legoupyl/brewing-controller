115a68dce1a3a6910bd643ec5506af1b19224e9a
xavfan <xavier.fanton@hotmail.fr>
2019-12-11 21:53:07 +0100

    ok

--- gui/bc-gui.py
+++ gui/bc-gui.py
@@ -10,8 +10,5 @@ img = Image.open("brasserie.bmp")
 tkimage = ImageTk.PhotoImage(img)
 
 
-T = tk.Text(root, height=2, width=30)
-T.pack()
-mlt_temp_label = tk.Label(root, text=mlt_temp)
 
 root.mainloop()
