073cf7a69afff3b1ca6b3da1a2489f58b77216e1
xavfan <xavier.fanton@hotmail.fr>
2019-12-15 10:52:17 +0100

    ok

--- gui/bc-gui.py
+++ gui/bc-gui.py
@@ -11,14 +11,18 @@ FontSize2=80
 y1=50
 y2=200
 y3= 400
-xMLT=50
-xBK=500
-xHLT= 800
+
+xMLT=35
+xBK=380
+xHLT= 730
+
+xOffset = 20
+
 
 # Set variable
 MLT_temp = 60.0
 
-BK_temp =98.2
+BK_temp = 62.1
 BK_power_setpoint = 85
 
 HLT_temp= 70.1
@@ -40,26 +44,52 @@ def gui():
 
     MLT_temp_label = tk.Label(panel1, text=MLT_temp,bg='black',font=("Helvetica", FontSize1),width=0,height=0,fg='orange')
     MLT_temp_label.place (x=xMLT,y=y1 )
+  
+
+    BK_temp_label = tk.Label(panel1, text=BK_temp,bg='black',font=("Helvetica", FontSize1),width=0,height=0,fg='orange')
+    BK_temp_label.place (x=xBK,y=y1 )
+    BK_power_setpoint_label=label = tk.Label(panel1, text=str (BK_power_setpoint) + "%",bg='black',font=("Helvetica", FontSize2),width=0,height=0,fg='white')
+    BK_power_setpoint_label.place (x=xBK+xOffset,y=y2 )
+
+    HLT_temp_label = tk.Label(panel1, text=HLT_temp,bg='black',font=("Helvetica", FontSize1),width=0,height=0,fg='orange')
+    HLT_temp_label.place (x=xHLT,y=y1 )
+    HLT_temp_setpoint_label=label = tk.Label(panel1, text=HLT_temp_setpoint,bg='black',font=("Helvetica", FontSize2),width=0,height=0,fg='white')
+    HLT_temp_setpoint_label.place (x=xHLT+xOffset,y=y2 )
+
 
     while True:
         time.sleep (1)
         MLT_temp_label.config (text=round (MLT_temp,1))
         MLT_temp_label.update()
-
+        BK_temp_label.config (text=round (BK_temp,1))
+        BK_temp_label.update()
+        HLT_temp_label.config (text=round (HLT_temp,1))
+        HLT_temp_label.update()
 
 
 
 
 def get_MLT_temp():
     global MLT_temp
+    global BK_temp
+    global HLT_temp
     while True:
         print (MLT_temp)
         time.sleep(1)
         if  MLT_temp < 70:
             MLT_temp=MLT_temp+0.1
+            BK_temp=BK_temp+0.1
+            HLT_temp=HLT_temp+0.1
+
+
         else:
             MLT_temp = 60.0        
             exit()
+
+
+
+
+
 
 
 MLT_temp_thread = threading.Thread(target=get_MLT_temp)
