b5bada72aeda536f9b1e940baec8dfd968095228
legoupyl <33391012+legoupyl@users.noreply.github.com>
2021-06-07 15:04:57 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,71 @@
+import os 
+import threading
+import tkinter as tk
+import time
+import sys
+import smbus 
+from gpiozero import Button 
+import i2cEncoderLibV2
+import max31865
+from simple_pid import PID
+import atexit
+from RPi import GPIO
+
+# set working directory do script directory
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+
+GPIO.setmode(GPIO.BCM)
+
+bus=smbus.SMBus(1)
+
+
+FontSize1=100
+FontSize2=80
+FontSize3=60
+
+y1=50
+y2=200
+y3= 350
+y4= 500
+
+xHLT=35
+xMLT=380
+xBK= 730
+
+xOffset = 20
+xOffset2 = 40
+
+# PT100 Calibration offset
+HLT_Calib_offset = -0.7
+MLT_Calib_offset = -0.5
+BK_Calib_offset = -1.1
+
+
+# Set wiring variable
+HLT_pt100_cspin = 18
+MLT_pt100_cspin = 19
+BK_pt100_cspin = 26
+
+
+HLT_heater_cspin=24 # or 24
+BK_heater_cspin=23
+
+HLT_encoder_cspin=0x10
+BK_encoder_cspin=0x07
+
+
+
+# Just for variables init
+HLT_temp = 0
+MLT_temp = 0
+BK_temp = 0
+
+HLT_temp_setpoint = 74
+MLT_temp_setpoint = 74
+BK_power_setpoint = 50
+
+
+MLT_REGUL_MODE = False
+
+BK_encoder = i2cEncoderLibV2.i2cEncoderLibV2(bus,BK_encoder_cspin)
